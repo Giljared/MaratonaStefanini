901ac0068fa026ae96cb75a48ae0bcbb89ece66c
Jonatas Do Vale <jonatasvalebr@gmail.com>
2020-10-06 09:38:45 -0300

    add: adiconando mais comentarios referente ao funcionamento do AngularJs

--- v1.0/app.js
+++ v1.0/app.js
@@ -15,9 +15,10 @@
 
     angular
     .module('app', [])
-    .controller('controlador',
-    function($scope){
-        $scope.nome = 'Joao';
+    .controller('controlador', // O primeiro parametro da funcao controller('controlador', **) tem que ser o mesmo que eh referenciado no ng-controller no index.html
+    function($scope){  //Recebe a variavel do scope que referencia a pagina index.html. Entao tudo q foi criado la via diretivas(ng-click,bind e outros) vem como um objeto atraves dessa variavel.
+        //console.log($scope); Para visualizar tudo que esesa variavel retorna para o controller
+        $scope.nome = 'Joao'; 
     })
     .controller('contador',
     function() {
